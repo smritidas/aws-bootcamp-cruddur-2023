cfa14c8aef3043480ac082945f34817cd11d606a
Smriti Das <dassmrit@gmail.com>
2024-03-05 21:32:34 +0000

    Add watchtower

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,11 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+import logging
+
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
+    LOGGER.info("HomeActivities")
     with tracer.start_as_current_span("home-activity-mock-data"):
       now = datetime.now(timezone.utc).astimezone()
       results = [{
